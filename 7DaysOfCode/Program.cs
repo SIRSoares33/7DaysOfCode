2dbab032ddd25400bc6a113c9fb2b4d19c44676e
SIRSoares33 <sirgustavo4@gmail.com>
2023-07-14 14:28:17 -0300

    Day 01

---
+++ 7DaysOfCode/Program.cs
@@ -0,0 +1,17 @@
+﻿using RestSharp;
+
+namespace _7DaysOfCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RestClient client = new RestClient("https://pokeapi.co/api/v2/pokemon/");
+            RestRequest request = new RestRequest("", Method.Get);
+
+            var response = client.Execute(request);
+
+            Console.WriteLine(response.Content);
+        }
+    }
+}
